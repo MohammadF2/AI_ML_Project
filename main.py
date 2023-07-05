0056e3b988159aa71e163a67b26690c572f1613c
Salah Tawafsha <93351227+SalahTawafsha@users.noreply.github.com>
2023-07-05 20:24:15 +0300

    v1

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
-from sklearn.tree import DecisionTreeClassifier  # Import Decision Tree Classifier
+from sklearn.tree import DecisionTreeClassifier, export_graphviz  # Import Decision Tree Classifier
 from sklearn.model_selection import train_test_split  # Import train_test_split function
 from sklearn import metrics
+from six import StringIO
 import pandas as pd
+from IPython.display import Image
+import pydotplus
 import collections
 
 
-def getData():
+def get_data():
     pd.set_option('display.max_columns', None)
     pd.set_option('display.width', 1000)
 
     col_names = ['NPG', 'PGL', 'DIA', 'TSF', 'INS', 'BMI', 'DPF', 'AGE', 'Diabetic']
-    dataRes = pd.read_csv('Data.csv', header=None, names=col_names)
-    return dataRes
+    data_res = pd.read_csv('Data.csv', header=None, names=col_names)
+    return data_res
 
 
-data = getData()
+data = get_data()
 
 
 def get_train_test(size):
@@ -27,7 +30,7 @@ def get_train_test(size):
     return features_train, features_test, target_train, target_test
 
 
-def getModel(features_train, target_train):
+def get_model(features_train, target_train):
     model = DecisionTreeClassifier()
 
     model = model.fit(features_train, target_train)
@@ -56,26 +59,37 @@ def printDistribution():
 
     # Print the percentages
     for k, v in proportions.items():
-        print(f'{k}: {v}%')
+        print(f'{k}: {v:.2f}%')
 
+def generate_decision_tree(model, data):
+    dot_data = StringIO()
+    feature_cols = ['NPG', 'PGL', 'DIA', 'TSF', 'INS', 'BMI', 'DPF', 'AGE']
+    export_graphviz(model, out_file=dot_data,
+                    filled=True, rounded=True,
+                    special_characters=True, feature_names=feature_cols, class_names=['0', '1'])
+    graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+    graph.write_png('diabetes.png')
+    Image(graph.create_png())
 
 # print Statistics
 print("Statistics:")
 printStatistics()
 
 # print distribution
-print("\nDistribution:")
+print("\nDistribution for percent of Diabetic:")
 printDistribution()
 
 # split data:
 features_train_M1, features_test_M1, target_train_M1, target_test_M1 = get_train_test(0.3)
 features_train_M2, features_test_M2, target_train_M2, target_test_M2 = get_train_test(0.5)
 
-M1 = getModel(features_train_M1, target_train_M1)
-M2 = getModel(features_train_M2, target_train_M2)
+M1 = get_model(features_train_M1, target_train_M1)
+M2 = get_model(features_train_M2, target_train_M2)
 
-print(f'\nModel 1 (30%): {getAccuracy(M1, features_test_M1, target_test_M1)}')
-print(f'Model 2 (50%): {getAccuracy(M2, features_test_M2, target_test_M2)}')
+# accuracy of each model
+print("\naccuracy of each model:")
+print('Model 1 (30%): {percent:.2f}'.format(percent=getAccuracy(M1, features_test_M1, target_test_M1) * 100))
+print('Model 2 (50%): {percent:.2f}'.format(percent=getAccuracy(M2, features_test_M2, target_test_M2) * 100))
 
-
-
+# generate decision trees
+generate_decision_tree(M1, data)
